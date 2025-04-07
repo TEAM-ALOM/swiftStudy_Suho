491c846ee07906ab6b2ad74d3793e73282e151ab
jxxngsxhx <jeongsuho0401@gmail.com>
2025-04-07 22:51:31 +0900

    3주차_과제_튜토리얼1

---
+++ OnboardingFlow/OnboardingFlow/FeaturesPage.swift
@@ -0,0 +1,33 @@
+//
+//  FeaturesPage.swift
+//  OnboardingFlow
+//
+//  Created by 정수호 on 4/7/25.
+//
+
+import SwiftUI
+
+struct FeaturesPage: View {
+    var body: some View {
+        VStack(spacing: 30) {
+            Text("Features")
+                .font(.title)
+                .fontWeight(.semibold)
+                .padding(.bottom)
+            
+            FeatureCard(iconName: "person.2.crop.square.stack.fill", description: "A multiline description about a feature paired with the image on the left.")
+            
+            FeatureCard(iconName: "quote.bubble.fill", description: "Short summary")
+            
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    FeaturesPage()
+        .frame(maxHeight: .infinity)
+        .background(Gradient(colors: gradientColors))
+        .foregroundStyle(.white)
+}
---
+++ OnboardingFlow/OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by 정수호 on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
