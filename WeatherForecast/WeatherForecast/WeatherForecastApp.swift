60ce32a263de3c5fca6cb9ee3401c5e64ab3539f
jxxngsxhx <jeongsuho0401@gmail.com>
2025-04-07 22:17:21 +0900

    2주차_과제

---
+++ WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by 정수호 on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
